65951c7dac1c41f605be9aba687b93a8b6512080
Araxia <corona.aria.a@gmail.com>
2025-02-17 01:42:35 -0500

    Add untitled helper module for frame creation in the UI

---
+++ src/untitled_helper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# Untitled Helper Module
+# Aria Corona Feb 17th, 2025
+
+import tkinter as tk
+from loggers.untitled_logger import logger
+
+class untitledHelper:
+    def __init__(self, conf='conf/conf.json'):
+        logger.debug("Initializing untitled helper module.")
+        self.conf = conf
+        
+        
+    def create_frame(self, parent, bg, borderwidth, relief, **kwargs):
+        border_frame = tk.Frame(
+            parent,
+            bg=bg,
+            borderwidth=borderwidth,
+            relief=relief,
+            **kwargs
+        )
+        
+        inner_frame = tk.Frame(
+            border_frame,
+            bg=bg,
+            **kwargs
+        )
+        
+        frame = {
+            'inner': inner_frame,
+            'border': border_frame
+        }
+        
+        return frame
