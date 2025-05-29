f9f9a6fda8426dec4812095c6675057a8126ec59
Aria Corona <corona.aria.a@gmail.com>
2025-05-29 18:01:50 -0400

    - Add common event types as attribute. - Add play_sound method.

--- untitled_rogue.py
+++ untitled_rogue.py
@@ -5,6 +5,7 @@ import json, pickle, sys
 from functools import wraps
 from src.loggers.untitled_logger import setup_logger
 from src.untitled_title_screen import UntitledTitleScreen
+from src.main_menu import MainMenu
 from src.untitled_helper import untitledHelper
 
 class UntitledRogueApp(tk.Tk):
@@ -13,12 +14,18 @@ class UntitledRogueApp(tk.Tk):
         self.logger = setup_logger()
         self.title("Untitled Rogue")
         self.geometry("800x600")
-        
         self.config = self.load_config(config_path)
         self.current_screen = "title"
         self.load_screen()
 
+
     def load_config(self, path):
+        self.event_types = [
+            "<Key>", "<KeyPress>", "<KeyRelease>", "<ButtonPress>", "<ButtonRelease>",
+            "<Motion>", "<Enter>", "<Leave>", "<FocusIn>", "<FocusOut>",
+            "<Configure>", "<Destroy>", "<Visibility>", "<Expose>",
+            "<Map>", "<Unmap>", "<Activate>", "<Deactivate>",
+        ]
         with open(path, 'r') as file:
             return json.load(file)
     
@@ -33,6 +40,7 @@ class UntitledRogueApp(tk.Tk):
     def load_screen(self):
         directory = {
             "title": self.load_title,
+            "main_menu": self.load_main_menu,
         }
         if self.current_screen in directory:
             self.logger.debug(f"Loading screen: {self.current_screen}")
@@ -43,9 +51,22 @@ class UntitledRogueApp(tk.Tk):
         
     @untitledHelper.clear_root
     def load_title(self):
-        title = UntitledTitleScreen(self, self.config, self.logger)
+        title = UntitledTitleScreen(self,)
         title.load_title()
     
+    @untitledHelper.clear_root
+    def load_main_menu(self):
+        main_menu = MainMenu(self,)
+        
+    def play_sound(self, sound):
+        """
+        Placeholder for sound playing functionality.
+        This method should be implemented to play sounds.
+        """
+        self.logger.debug(f"Playing sound: {sound.name}")
+        # Implement sound playing logic here
+        pass
+        
     
 if __name__ == "__main__":
     app = UntitledRogueApp()
